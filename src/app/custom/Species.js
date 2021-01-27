2b014d51cdb64248cfef898c9c56e7885a023063
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-01-27 19:38:19 +0200

    feat: start working on species task

---
+++ src/app/custom/Species.js
@@ -0,0 +1,21 @@
+import EventEmitter from "eventemitter3";
+
+export default class Species extends EventEmitter {
+  static get events() {
+    return { SPECIES_CREATED: "species_created" };
+  }
+
+  constructor() {
+    super();
+    this.name = null;
+    this.classification = null;
+  }
+
+  async init(url) {
+    const currentSpeciesRaw = await fetch(url);
+    const currentSpeciesResolved = await currentSpeciesRaw.json();
+    this.name = currentSpeciesResolved.name;
+    this.classification = currentSpeciesResolved.classification;
+    this.emit("SPECIES_CREATED");
+  }
+}
